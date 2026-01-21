269283757cc4570ecab291a4ac3dda98462d18af
vitor <vitormwxm@gmail.com>
2026-01-20 22:54:55 -0300

    first commit

---
+++ src/test/java/com/vitormwxm/modelo_dominio_orm/ModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitormwxm.modelo_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
